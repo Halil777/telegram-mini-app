120b62694260a1a30586da1d7d482b8998cdddea
Halil777 <microsoft779@gmail.com>
2024-09-09 13:41:27 +0500

    added link domain

--- Bot/bot.ts
+++ Bot/bot.ts
@@ -1,15 +1,15 @@
-// import { Telegraf } from "telegraf";
-// const TOKEN = "6708675445:AAEbyNIVaUiiPQhPwb2BRCtRIoJIEXvevGg";
-// const bot = new Telegraf(TOKEN);
+import { Telegraf } from "telegraf";
+const TOKEN = "6708675445:AAEbyNIVaUiiPQhPwb2BRCtRIoJIEXvevGg";
+const bot = new Telegraf(TOKEN);
 
-// const web_link = "https://celebrated-torte-184681.netlify.app/";
+const web_link = "https://telegram-mini-app-v7oj.vercel.app/";
 
-// bot.start((ctx) =>
-//   ctx.reply("Welcome :)))))", {
-//     reply_markup: {
-//       keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
-//     },
-//   })
-// );
+bot.start((ctx) =>
+  ctx.reply("Welcome :)))))", {
+    reply_markup: {
+      keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
+    },
+  })
+);
 
-// bot.launch();
+bot.launch();
