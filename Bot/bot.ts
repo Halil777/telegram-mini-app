3454f2f3fc6525890ce539c7b35c00d0fc22b87c
Halil777 <microsoft779@gmail.com>
2024-09-09 13:34:50 +0500

    added bot folder

---
+++ Bot/bot.ts
@@ -0,0 +1,15 @@
+// import { Telegraf } from "telegraf";
+// const TOKEN = "6708675445:AAEbyNIVaUiiPQhPwb2BRCtRIoJIEXvevGg";
+// const bot = new Telegraf(TOKEN);
+
+// const web_link = "https://celebrated-torte-184681.netlify.app/";
+
+// bot.start((ctx) =>
+//   ctx.reply("Welcome :)))))", {
+//     reply_markup: {
+//       keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
+//     },
+//   })
+// );
+
+// bot.launch();
